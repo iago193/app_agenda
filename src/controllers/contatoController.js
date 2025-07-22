8e75194b44b20fb80c9fd07ec5ff4cf7275b700d
iago bruno <iago.silva6969@gmail.com>
2025-07-22 20:18:33 -0300

    +

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,7 +1,11 @@
+import Contato from '../model/contatoModel.js';
+
 export function indexContato(req, res) {
     res.render('contato');
 }
 
-export function registerContato(req, res) {
+export async function registerContato(req, res) {
+    const contato = new Contato(req.body);
+    await contato.register();
     res.send('oi')
 }
--- src/model/contatoModel.js
+++ src/model/contatoModel.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const ContatoSchema = new mongoose.Schema({
-  titulo: {type: String, required: true },
-  descricao: String
+    nome: { type: String, required: true },
+    sobrenome: { type: String, required: false, default: '' },
+    email: { type: String, required: false, default: '' },
+    telefone: { type: String, required: false, default: '' },
+    criadoEm: {type: Date, default: Date.now}
 });
 
 // Modelo exportado
@@ -13,6 +17,34 @@ class Contato {
         this.body = body;
         this.errors = [];
         this.contato = null;
+    }
+
+    async register() {
+        this.valida();
+        if(this.errors > 0) return;
+        this.contato = await ContatoModel.create(this.body);
+    }
+
+    valida() {
+        this.cleanUp();
+
+        if (this.body.email && !validator.isEmail(this.body.email)) {
+            this.errors.push('E-mail inválido.');
+        }
+        if(!this.body.nome) this.errors.push('Nome é um campo obrigatório.');
+        if(!this.body.nome && !this.body.email) {
+            this.errors.push('Pelo menos um contato precisa ser enviado: e-mail ou telefone');
+        }
+    }
+
+    cleanUp() {
+
+        this.body = {
+            nome: this.body.nome,
+            sobrenome: this.body.sobrenome,
+            email: this.body.email,
+            telefone: this.body.telefone,
+        };
     }
 }
 
